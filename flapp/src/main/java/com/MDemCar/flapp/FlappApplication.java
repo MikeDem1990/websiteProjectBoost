00c457548b2561902d3ac045eb4862e78bb4e032
Mike <51278765+MikeDem1990@users.noreply.github.com>
2022-06-09 11:19:08 +0300

    Add files via upload

---
+++ flapp/src/main/java/com/MDemCar/flapp/FlappApplication.java
@@ -0,0 +1,13 @@
+package com.MDemCar.flapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  FlappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlappApplication.class, args);
+	}
+
+}
